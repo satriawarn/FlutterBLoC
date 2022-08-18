eed5b02eb28ff02fe2c57d6f53ef8ddb85e9d472
Erik Satriawan <erik.satriawan@mitrais.com>
2022-08-18 16:19:56 +0700

    update timer bloc

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-import 'counter/view/counter_page.dart';
+import 'timer/view/timer_page.dart';
 
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: const TimerPage(),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_state_bloc/app.dart';
-import 'package:flutter_state_bloc/counter_observer.dart';
+import 'app.dart';
 
 void main() {
-  Bloc.observer = CounterObserver();
-  runApp(const CounterApp());
+  runApp(const App());
 }
