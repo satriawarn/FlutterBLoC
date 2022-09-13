22ec9661f131aa85b616724b25b8abfec607fbd4
Erik Satriawan <satriawarn@gmail.com>
2022-09-13 15:54:00 +0700

    adding unit and widget tests

--- lib/app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'counter/view/counter_page.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,35 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_state_bloc/app.dart';
-import 'package:flutter_state_bloc/counter_observer.dart';
+import 'package:flutter_state_bloc/screens/favorites.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_state_bloc/models/favorites.dart';
+
+import 'screens/home.dart';
 
 void main() {
-  Bloc.observer = CounterObserver();
-  runApp(const CounterApp());
+  runApp(const TestingApp());
+}
+
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => const HomePage(),
+          FavoritePage.routeName: (context) => const FavoritePage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
 }
