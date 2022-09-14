0f7e5f7d4c790359d9cd13a44250e542a1d2bcee
Erik Satriawan <satriawarn@gmail.com>
2022-09-14 08:57:41 +0700

    update integration test and tap,drag and enter text test

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,60 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_state_bloc/screens/favorites.dart';
-import 'package:provider/provider.dart';
-
-import 'package:flutter_state_bloc/models/favorites.dart';
-
-import 'screens/home.dart';
 
 void main() {
   runApp(const TestingApp());
 }
 
-class TestingApp extends StatelessWidget {
+class TestingApp extends StatefulWidget {
   const TestingApp({super.key});
 
   @override
+  State<TestingApp> createState() => _TestingAppState();
+}
+
+class _TestingAppState extends State<TestingApp> {
+  static const _appTitle = 'Todo List';
+  final todos = <String>[];
+  final controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Favorites>(
-      create: (context) => Favorites(),
-      child: MaterialApp(
-        title: 'Testing Sample',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MaterialApp(
+      title: _appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_appTitle),
         ),
-        routes: {
-          HomePage.routeName: (context) => const HomePage(),
-          FavoritePage.routeName: (context) => const FavoritePage(),
-        },
-        initialRoute: HomePage.routeName,
+        body: Column(
+          children: [
+            TextField(
+              controller: controller,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+
+                  return Dismissible(
+                    key: Key('$todo$index'),
+                    onDismissed: (direction) => todos.removeAt(index),
+                    background: Container(color: Colors.red),
+                    child: ListTile(title: Text(todo)),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              todos.add(controller.text);
+              controller.clear();
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
---
+++ test/main_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_bloc/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('widget testing', () {
+    testWidgets('Add and remove a todo', (tester) async {
+      // Build the widget.
+      await tester.pumpWidget(const TestingApp());
+
+      // Enter 'hi' into the TextField.
+      await tester.enterText(find.byType(TextField), 'hi');
+
+      // Tap the add button.
+      await tester.tap(find.byType(FloatingActionButton));
+
+      // Rebuild the widget with the new item.
+      await tester.pump();
+
+      // Expect to find the item on screen.
+      expect(find.text('hi'), findsOneWidget);
+
+      // Swipe the item to dismiss it.
+      await tester.drag(find.byType(Dismissible), const Offset(500.0, 0.0));
+
+      // Build the widget until the dismiss animation ends.
+      await tester.pumpAndSettle();
+
+      // Ensure that the item is no longer on screen.
+      expect(find.text('hi'), findsNothing);
+    });
+  });
+}
